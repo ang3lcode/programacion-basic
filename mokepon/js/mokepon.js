f46c30a42a7e1cce50b261b460050d6a32105215
Luis Angel Perez Melendez <angelmelen88@gmail.com>
2022-08-14 20:05:24 -0500

    tercer commit clase 21

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,7 +1,20 @@
-function seleccionarMascotaJugador () {
-    alert('hola');
+function iniciarJuego(){
+    let botonMascotaJugador=document.getElementById('boton-mascota');
+    botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador);
 }
-
-let botonMascota = document.getElementById('boton-mascota');
-botonMascota = addEventListener('click', seleccionarMascotaJugador);
-
+function seleccionarMascotaJugador(){
+    let inputHipodoge=document.getElementById('hipodoge')
+    let inputCapipepo=document.getElementById('capipepo')
+    let inputRatigueya=document.getElementById('ratigueya')
+    
+    if (inputHipodoge.checked){
+        alert('hipodoge')
+    } else if(inputCapipepo.checked){
+        alert('Capipepo')
+    } else if (inputRatigueya.checked){
+        alert('Ratigueya')
+    } else {
+        alert('solecciona uno')
+    }
+}
+window.addEventListener('load',iniciarJuego);
