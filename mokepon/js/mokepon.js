64eabfb3b953a54db97cea6948420b01ab80576a
Luis Angel Perez Melendez <angelmelen88@gmail.com>
2022-08-14 19:13:22 -0500

    commit 3 js

---
+++ mokepon/js/mokepon.js
@@ -0,0 +1,7 @@
+function seleccionarMascotaJugador () {
+    alert('hola');
+}
+
+let botonMascota = document.getElementById('boton-mascota');
+botonMascota = addEventListener('click', seleccionarMascotaJugador);
+
