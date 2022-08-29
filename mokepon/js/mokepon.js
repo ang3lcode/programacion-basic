a59a24dd3bf3e73a7bbf082b9d5f54340c326d98
Luis Angel Perez Melendez <angelmelen88@gmail.com>
2022-08-28 19:46:13 -0500

    me complique aqui

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -21,6 +21,7 @@ const contenedorAtaques = document.getElementById('contenedorAtaques')
 const sectionVerMapa = document.getElementById('ver-mapa')
 const mapa = document.getElementById('mapa')
 
+let jugadorId = null
 let mokepones = []
 let ataqueJugador =[]
 let ataqueEnemigo = []
@@ -157,8 +158,22 @@ function iniciarJuego() {
     })
     
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
-
     botonReiniciar.addEventListener('click', reiniciarJuego)
+
+    unirseAlJuego()
+}
+function unirseAlJuego() {
+    fetch("http://localhost:8080/unirse")
+        .then(function(res){
+            // console.log(res)
+            if(res.ok){
+                res.text()
+                .then(function(respuesta) {
+                    console.log(respuesta)
+                    jugadorId = respuesta
+                })
+            }
+        })
 }
 function seleccionarMascotaJugador() {    
     sectionSeleccionarMascota.style.display = 'none'    
@@ -176,12 +191,21 @@ function seleccionarMascotaJugador() {
     } else {
         alert('Selecciona una mascota')
     }
-
+    seleccionarMokepon(mascotaJugador)
     extraerAtaques(mascotaJugador)
-    sectionVerMapa.style.display = 'flex'  
-    
-    iniciarMapa()
-    
+    sectionVerMapa.style.display = 'flex'     
+    iniciarMapa()    
+}
+function seleccionarMokepon(mascotaJugador){
+    fetch(`http://localhost:8080/mokepon/${jugadorId}`,{
+        method:"post",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+            mokepon:mascotaJugador
+        })
+    })
 }
 function extraerAtaques(mascotaJugador) {
     let ataques
